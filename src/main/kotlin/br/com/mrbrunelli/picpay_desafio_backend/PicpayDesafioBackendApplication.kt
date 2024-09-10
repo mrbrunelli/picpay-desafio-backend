3e8266dc93a4616674c6c0ed6d50d57b7af8147f
Brunelli <matheus.brunelli@compasso.com.br>
2024-09-10 16:57:30 -0300

    configure topic builder

--- src/main/kotlin/br/com/mrbrunelli/picpay_desafio_backend/PicpayDesafioBackendApplication.kt
+++ src/main/kotlin/br/com/mrbrunelli/picpay_desafio_backend/PicpayDesafioBackendApplication.kt
@@ -1,8 +1,11 @@
 package br.com.mrbrunelli.picpay_desafio_backend
 
+import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
+import org.springframework.kafka.config.TopicBuilder
 
 @EnableJdbcAuditing
 @SpringBootApplication
@@ -10,4 +13,9 @@ class PicpayDesafioBackendApplication
 
 fun main(args: Array<String>) {
 	runApplication<PicpayDesafioBackendApplication>(*args)
+}
+
+@Bean
+fun notificationTopic(): NewTopic {
+	return TopicBuilder.name("transaction-notification").build()
 }
